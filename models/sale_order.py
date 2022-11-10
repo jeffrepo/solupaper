7275010de9cab88fb72d8d17a62e0174269a1caf
jeffrepo <jeffer.leo93@gmail.com>
2022-11-10 16:31:27 -0600

    Cambios varios

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,7 +7,7 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     po_customer = fields.Char("PO cliente")
-    po_customer_name = fields.Char("Po nombre cliente")
+    po_customer_name = fields.Char("PO nombre cliente")
     solupaper_incoterm_id = fields.Many2one("solupaper.incoterm","Solupaper incoterm")
     puerto = fields.Char("Puerto")
     requerido = fields.Char("requerido")
