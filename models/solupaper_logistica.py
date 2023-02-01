84d5ad4ac7932703a03f996e3402053ed5d6034b
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2023-02-01 13:50:17 -0600

    Update solupaper_logistica.py

--- models/solupaper_logistica.py
+++ models/solupaper_logistica.py
@@ -14,11 +14,11 @@ class SolupaperLogistica(models.Model):
     company_id = fields.Many2one('res.company',
         required=True,
         default=lambda self: self.env.company)
-    proforma_id = fields.Many2one("sale.order", "Proforma:")
+    proforma_id = fields.Many2one("sale.order", "Proforma")
     quantity_containers = fields.Integer("Quantity of containers")
     type_container = fields.Selection([('a', " 20' "), ('b', "40'DC"), ('c', "40'HC"), ('d', "40 NOR")])
     days_free = fields.Selection([('a', '7'), ('b', '14'), ('c', '21')])
-    place_deliver = fields.Text(string='Place of  Delivery in Case of  DAP:')
+    place_deliver = fields.Text(string='Place of  Delivery in Case of  DAP')
     purchase_order = fields.Many2one("purchase.order", "Purchase order")
     incoterm_id = fields.Many2one("solupaper.incoterm","Solupaper incoterm")
     freight_supplier_id = fields.Many2one('res.partner', "Freigth Forward Information")
